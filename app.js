5e08b48d0d069722e022ff4e5095f05267c375c6
Patrick Eubanks <eubanksps@s.dcsdk12.org>
2016-03-09 12:30:45 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+$(document).ready(function()	{
+	
+	$('.a').on('click', function()	{
+		
+		$('.answer').text('Chillary Clinton');
+		
+		$('.answer').addClass('incorrect');
+		
+		$('.right').text('INCORRECT');
+		
+	});
+	
+	$('.b').on('click', function()	{
+		
+		$('.answer').text('Donald "The Hair" Trump');
+		
+		$('.answer').addClass('correct');
+		
+		$('.right').text('CORRECT');
+		
+		$('.question').append('<button>Go On<button>');
+		
+	});
+	
+	$('.c').on('click', function()	{
+		
+		$('.answer').text('Michael Jackson');
+		
+		$('.answer').addClass('incorrect');
+		
+		$('.right').text('INCORRECT');
+		
+	});
+	
+	$('.d').on('click', function()	{
+		
+		$('.answer').text('Pepe Le Frog');
+		
+		$('.answer').addClass('incorrect');
+		
+		$('.right').text('INCORRECT');
+		
+	});
+	
+	$('button').on('click', function()	{
+		
+			
+		
+	};
+	
+});
